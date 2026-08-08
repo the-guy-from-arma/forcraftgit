f7e4678e5530d4cc0c184067f8bf24b0819e0c74
documented-illegal-alien <blacksheepgaming15@gmail.com>
2026-08-07 23:54:05 -0400

    cccc
    cccccc

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE = "faircroft-rp-v030-casino-motion";
+const CACHE = "faircroft-rp-v030-casino-tools-guide";
 const ASSETS = [
   "/",
-  "/static/styles.css?v=0.3.0-casino-motion",
+  "/static/styles.css?v=0.3.0-casino-tools-guide",
   "/static/roadmap.css?v=0.1.1-ops4",
-  "/static/app.js?v=0.3.0-casino-motion",
+  "/static/app.js?v=0.3.0-casino-tools-guide",
   "/static/brand/platforms/windows.svg",
   "/static/brand/platforms/xbox.svg",
   "/static/brand/platforms/playstation.svg",
